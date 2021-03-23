0c604a8db02fe421887d9336589d80a7115daa39
Tomas Brezny <brezny.tomas@gmail.com>
2021-03-23 19:17:08 +0100

    Update UserName.java

--- ChatSystem (Server)/src/model/UserName.java
+++ ChatSystem (Server)/src/model/UserName.java
@@ -9,7 +9,7 @@ public class UserName
     if (username == null || username.length() < 3)
       throw new IllegalArgumentException("Username must have at least 3 characters");
     if (username.length() > 20)
-      throw new IllegalArgumentException("Username cannot have more than 100 characters");
+      throw new IllegalArgumentException("Username cannot have more than 20 characters");
 
     this.name = username;
   }
