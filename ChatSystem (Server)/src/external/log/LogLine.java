477591d13bb6e635d8528874a3ef629ceaffadb9
GeraltWitcher1 <57898274+GeraltWitcher1@users.noreply.github.com>
2021-03-23 18:05:37 +0100

    Added log

---
+++ ChatSystem (Server)/src/external/log/LogLine.java
@@ -0,0 +1,27 @@
+package external.log;
+
+public class LogLine
+{
+  private DateTime dateTime;
+  private String text;
+
+  public LogLine(String text)
+  {
+    this.text = text;
+    this.dateTime = new DateTime();
+  }
+
+  public DateTime getDateTime()
+  {
+    return dateTime;
+  }
+
+  public String getText()
+  {
+    return text;
+  }
+
+  @Override public String toString(){
+    return String.format("%s: %s",dateTime,text);
+  }
+}
---
+++ ChatSystem (Server)/src/external/log/TestLog.java
@@ -0,0 +1,9 @@
+package external.log;
+
+public class TestLog
+{
+  public static void main(String[] args)
+  {
+    Log.getLog().addLog("Hello");
+  }
+}
