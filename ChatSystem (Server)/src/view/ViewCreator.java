eead4a4101838a3aeabad2923d259885509862f9
Alex <abistrovs@gmail.com>
2021-03-23 19:33:41 +0100

    updated viewcreator and viewhandler - still needs work

--- ChatSystem (Server)/src/view/ViewCreator.java
+++ ChatSystem (Server)/src/view/ViewCreator.java
@@ -1,5 +1,24 @@
 package view;
 
-public class ViewCreator
+import javafx.scene.layout.Region;
+
+public abstract class ViewCreator
 {
+
+  public ViewCreator()
+  {
+
+  }
+
+  public ViewController getViewController()
+  {
+
+  }
+
+  protected abstract void initViewController(ViewController controller, Region root);
+
+  private ViewController loadFromFXML(String fxmlFile)
+  {
+
+  }
 }
